3fb0ce4718992028f020ad249997895da660aa90
Rehan Jaffer <ray@thelondonvandal.com>
2018-02-24 18:38:06 +0000

    removed unused shuffle function

--- predict.js
+++ predict.js
@@ -2,16 +2,6 @@ var fs = require('fs');
 var eachCons = require('each-cons');
 var getStdin = require('get-stdin');
 
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-}
-
 class Predictor {
   constructor(corpus) {
     this.corpus = corpus;
