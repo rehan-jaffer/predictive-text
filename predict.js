5ea5dfe8ab0cbaed07e95165b23b4ff43d53e10d
Rehan Jaffer <ray@thelondonvandal.com>
2018-02-24 18:36:43 +0000

    kinda works now, this mechanism should work, implement into keyboard interface to improve further

--- predict.js
+++ predict.js
@@ -1,5 +1,16 @@
 var fs = require('fs');
 var eachCons = require('each-cons');
+var getStdin = require('get-stdin');
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+}
 
 class Predictor {
   constructor(corpus) {
@@ -8,12 +19,40 @@ class Predictor {
     let words = corpus.split(" ");
     let word_pairs = eachCons(words, 2);
     word_pairs.forEach((pair) => 
-      this.add(pair) );
+      this.add(pair[0], pair[1]) );
   }
-  add(w1, w2) {
+  add(w1,w2) {
     if (!this.words[w1]) {
       this.words[w1] = {};
     }
+    if (!this.words[w1][w2]) {
+      this.words[w1][w2] = 0;
+    }
+    this.words[w1][w2] += 1;
+  }
+
+  get(word) {
+    if (!this.words[word]) {
+      return "";
+    }
+    var followers = Object.entries(this.words[word]).sort((k) => k[1]);
+    followers = followers.sort();
+    let sum = followers.reduce((sum,kv) => sum += kv, 0);
+    let random = Math.random(sum)+1
+    let partial_sum = 0
+    let c = 0;
+    let choices = [];
+    for(let x=0;x<5;x++) {
+      choices.push(followers[x][0]);
+    }
+    console.log(choices.join(" "));
+    while (partial_sum < random && c < followers.size) {
+      console.log(followers[c]);
+      c++;
+      partial_sum += followers[c][1];
+    }
+
+    return followers[c][0];
   }
 
 }
@@ -21,3 +60,9 @@ class Predictor {
 var corpus = fs.readFileSync('tng.txt', 'utf8');
 
 pred = new Predictor(corpus);
+
+process.stdin.on('data', function (data) {
+  console.log(data.toString());
+  w = pred.get(data.toString().trim());
+  console.log(w);
+});
